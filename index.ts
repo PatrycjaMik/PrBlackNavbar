3f43b95eb9b7bf26bf79513222be992ff897b6f2
PatrycjaMik <80001646+PatrycjaMik@users.noreply.github.com>
2022-08-31 16:24:20 +0200

    mocked database

---
+++ index.ts
@@ -0,0 +1,162 @@
+export const NavbarData = [
+  {
+      id: 1,
+      name: 'Jedynka', 
+      href: 'https://polskieradio.pl/7,Jedynka',
+      color: 'hoverOrange',
+  },
+  {
+      id: 2,
+      name: 'Dwójka', 
+      href: 'https://polskieradio.pl/8,Dwojka',
+      color: 'hoverGreen',
+  },
+
+  {
+      id: 3,
+      name: 'Trójka', 
+      href: 'https://polskieradio.pl/9,Trojka',
+      color: 'hoverPink',
+  },
+   {
+      id: 4,
+      name: 'Czwórka', 
+      href: 'https://polskieradio.pl/10,Czworka',
+      color: 'hoverYellow',
+  },
+
+   {
+      id: 5,
+      name: 'PR24', 
+      href: 'https://polskieradio24.pl/',
+      color: 'hoverRed',
+  },
+  {
+      id: 6,
+      name: 'Poland', 
+      href: 'https://polskieradio.pl/395',
+      color: 'hoverRed',
+  },
+  {
+      id: 7,
+      name: 'Kierowcy', 
+      href: 'https://radiokierowcow.pl/',
+      color: 'hoverRed',
+  },
+
+  {
+      id: 8,
+      name: 'Dzieci', 
+      href: 'https://polskieradio.pl/18,Dzieci',
+      color: 'hoverBlue',
+  },
+   {
+      id: 9,
+      name: 'Chopin', 
+      href: 'https://chopin.polskieradio.pl/',
+      color: 'hoverWhite',
+  },
+
+   {
+      id: 10,
+      name: 'Podcasty', 
+      href: 'https://podcasty.polskieradio.pl/',
+      color: 'hoverRed',
+  },
+];
+
+export const MoreMediaLeft = [
+  {
+    id: 1, 
+    name: 'Kanały internetowe',
+    href: 'http://moje.polskieradio.pl',
+    class: 'moreMedia',
+    src: '/icons/kanalyinternetowe.svg',
+  },
+  {
+    id: 2, 
+    name: 'Serwisy historyczne',
+    href: 'https://www.polskieradio.pl/219',
+    class: 'moreMedia',
+    src: '/icons/serwisyhistoryczne.svg',
+  },
+  {
+    id: 3, 
+    name: 'RCKL',
+    href: 'https://www.polskieradio.pl/377',
+    class: 'moreMedia',
+    src: '/icons/rckl.svg',
+  },
+];
+
+export const middleColumnOne = [
+  {
+    id: 1, 
+    name: 'Studio reportażu i dokumentu Polskiego Radia',
+    href: 'https://reportaz.polskieradio.pl/',
+    class: 'moreMedia',
+    src: '/icons/studioreportazu.svg',
+  },
+  {
+    id: 2, 
+    name: 'Teatr Polskiego Radia',
+    href: 'https://www.polskieradio.pl/357',
+    class: 'moreMedia',
+    src: '/icons/teatrpr.svg',
+  },
+  {
+    id: 3, 
+    name: 'Orkiestra Polskiego Radia w Warszawie',
+    href: 'https://www.polskieradio.pl/219',
+    class: 'moreMedia',
+    src: '/icons/orkiestra.svg',
+  },
+];
+
+export const middleColumnTwo = [
+  {
+    id: 1, 
+    name: 'Ramówka',
+    href: 'https://polskieradio.pl/ramowka/Dzien/2022-03-29',
+    class: 'moreMedia',
+    src: '/icons/ramowka.svg',
+  },
+  {
+    id: 2, 
+    name: 'Częstotliwości',
+    href: 'https://www2.polskieradio.pl/czestotliwosci/',
+    class: 'moreMedia',
+    src: '/icons/czestotliwosci.svg',
+  },
+  {
+    id: 3, 
+    name: 'Lektury do słuchania',
+    href: 'https://www.polskieradio.pl/211,Powiesci-na-antenie',
+    class: 'moreMedia',
+    src: '/icons/lektury.svg',
+  },
+];
+
+export const rightColumn = [
+  {
+    id: 1, 
+    name: 'Sklep',
+    href: 'https://sklep.polskieradio.pl/',
+    class: 'moreMedia',
+    src: '/icons/sklep.svg',
+  },
+  {
+    id: 2, 
+    name: 'Szkolenia',
+    href: 'https://www.polskieradio.pl/275,Szkolenia',
+    class: 'moreMedia',
+    src: '/icons/szkolenia.svg',
+  },
+  {
+    id: 3, 
+    name: 'Akademia radiowa',
+    href: 'https://www.polskieradio.pl/275/8869,Akademia',
+    class: 'moreMedia',
+    src: '/icons/akademiaradiowa.svg',
+  },
+]
